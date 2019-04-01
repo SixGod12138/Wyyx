d32fad82816df1083457c39d570ec1fa1facad51
SixGod12138 <46626436+SixGod12138@users.noreply.github.com>
2019-04-01 10:18:16 +0800

    Add files via upload

---
+++ 网易严选/js/index.js
@@ -0,0 +1,18 @@
+;(function(){
+	
+		var swiper=new Swiper('.swiper-container',{
+			spaceBetween:true,
+			centeredSlides:true,
+			autoplay:{
+				delay:2000,
+				disableOnInteraction:false,
+				
+			},
+			
+			dierction:'horizontal',
+			loop:true,
+			
+		})
+	
+	
+})();
